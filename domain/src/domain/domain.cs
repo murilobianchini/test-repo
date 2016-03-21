aa511ce81a5b4182ac80cfb7ea48799587633420
zergsbo <murilo.bianchini@gmail.com>
2016-03-21 12:28:25 -0300

    domain

---
+++ domain/src/domain/domain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.test.domain
+{
+    public class Domain
+    {
+        public string test;
+
+        public Domain(string text)
+        {
+            this.test = text;
+            this.test = "the string serves no purpose";
+            this.test = "domain";
+        }
+    }
+}
