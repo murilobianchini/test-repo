018777bd4b5e2373b739d643f1f2d61f9c52bfa8
gauchoj <j.canal@gmail.com>
2016-03-21 10:17:29 -0300

    test commit

---
+++ engine/engine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.assukar.engine
+{
+    public class Engine
+    {
+        public string test;
+
+        public Engine(string text)
+        {
+            this.test = text;
+        }
+    }
+}
